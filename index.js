38cb94914b788aad22cf5a28f1c7788962d70988
cormorecheleo <leocormo@gmail.com>
2023-01-05 14:57:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+const cors = require("cors");
+const cookieSession = require("cookie-session")
+
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(
+    cookieSession({
+        name:"standfor-session",
+        secret:"F1",
+        httpOnly:true
+    })
+)
+
+app.get('/', (req,res) => {
+    res.json({message: "Welcome to gaming ambition api"})
+});
+
+app.listen(port, () => {
+    console.log('App launch on port ',port)
+})
